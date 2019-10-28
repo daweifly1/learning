f2e10c90940030db285318c33a31e51920ca759e
chendawei <chendawei>
2019-10-28 14:07:58 +0800

    init nacos 成功和前端交互

--- proj/gateway-server/src/main/java/yb/ecp/fast/infra/web/CasController.java
+++ proj/gateway-server/src/main/java/yb/ecp/fast/infra/web/CasController.java
@@ -3,6 +3,7 @@ package yb.ecp.fast.infra.web;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,7 +22,7 @@ public class CasController {
     @Value("${cas.defaultUrl:/cas}")
     private String defaultUrl;
     //
-//    @Autowired
+    @Autowired
     private CasClient casClient;
 
     private Logger logger = LoggerFactory.getLogger(CasController.class);
